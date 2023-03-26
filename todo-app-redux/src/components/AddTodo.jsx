b0b7ccada49a35fe4e55775eded37e94fe6b31cd
rahman <margub.9363@gmail.com>
2023-03-26 12:38:16 +0530

    28.00

--- todo-app-redux/src/components/AddTodo.jsx
+++ todo-app-redux/src/components/AddTodo.jsx
@@ -1,31 +1,53 @@
-import { Container, Row, Col, Card, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 
-const AddTodo = () => (
-  <Container>
-    <Row>
-      <Col md={12}>
-        <Card className="shadow-sm">
-          <Card.Body>
-            <h3>Add Todo here!</h3>
-            {/* form */}
-            <Form>
-              {/* title */}
-              <Form.Group>
-                <Form.Label>Todo Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter here" />
-              </Form.Group>
+const AddTodo = () => {
+  const [todo, setTodo] = useState({
+    title: "",
+    description: "",
+  });
 
-              {/* title */}
-              <Form.Group>
-                <Form.Label>Todo Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter here" />
-              </Form.Group>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+  return (
+    <Container>
+      <Row>
+        <Col md={12}>
+          <Card className="shadow-sm">
+            <Card.Body>
+              <h3>Add Todo here!</h3>
+              {/* form */}
+              <Form>
+                {/* title */}
+                <Form.Group>
+                  <Form.Label>Todo Title</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter here"
+                    value={todo.title}
+                    onChange={(event) =>
+                      setTodo({ ...todo, title: event.target.value })
+                    }
+                  />
+                </Form.Group>
+
+                {/* description */}
+                <Form.Group className="mt-3">
+                  <Form.Label>Todo Description</Form.Label>
+                  <Form.Control
+                    as={"textarea"}
+                    type="text"
+                    placeholder="Enter here"
+                  />
+                </Form.Group>
+                <Container className="text-center mt-3">
+                  <Button variant="primary">Add Todo</Button>
+                </Container>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default AddTodo;
