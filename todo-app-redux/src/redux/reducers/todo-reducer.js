901b52786ece279f857b9c50646c1730e13501fd
rahman <margub.9363@gmail.com>
2023-03-26 13:59:31 +0530

    52:00

---
+++ todo-app-redux/src/redux/reducers/todo-reducer.js
@@ -0,0 +1,38 @@
+import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from "../actions/action-types";
+
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      // add todo
+      return [...state, action.payload];
+    case DELETE_TODO:
+      // delete todo
+      const newState = state.filter((todo) => todo.id !== action.payload);
+      return newState;
+    case UPDATE_TODO:
+      // update todo
+      const updateState = state.map((todo) => {
+        if (todo.id == action.payload.todoId) {
+          todo.title = action.payload.todo;
+          todo.description = action.payload.todo.description;
+
+          return todo;
+        }
+      });
+      return updateState;
+    default:
+      return state;
+  }
+
+  //   if (action.type == ADD_TODO) {
+  //     // add todo
+  //   } else if (action.type == DELETE_TODO) {
+  //     // delete Todo
+  //   } else if (action.type == UPDATE_TODO) {
+  //     // update todo
+  //   } else {
+  //     // return initial state
+  //   }
+};
