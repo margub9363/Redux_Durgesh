288100c59205c877719a7084ce6bbf1ef12e8be0
rahman <margub.9363@gmail.com>
2023-03-26 13:14:08 +0530

    34:00

--- todo-app-redux/src/components/DisplayTodos.jsx
+++ todo-app-redux/src/components/DisplayTodos.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-import { Card, Col, Row, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Col, Row, Container, ListGroup } from "react-bootstrap";
 
 const DisplayTodos = () => {
+  const [todos, setTodos] = useState([
+    {
+      title: "first Titel",
+      description: "first Description",
+    },
+    {
+      title: "Second Titel",
+      description: "Second Description",
+    },
+  ]);
+
   return (
     <Container>
       <Row>
@@ -9,6 +20,14 @@ const DisplayTodos = () => {
           <Card className="mt-3 shadow-sm">
             <Card.Body>
               <h3>All Todos are here</h3>
+              <ListGroup>
+                {todos.map((todo, index) => (
+                  <ListGroup.Item key={index}>
+                    <h4>{todo.title}</h4>
+                    <p>{todo.description}</p>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
             </Card.Body>
           </Card>
         </Col>
